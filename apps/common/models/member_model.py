703549182dc0e42ac0d27654d348b96fc5cce415
ddusi <58924238+ddusi@users.noreply.github.com>
2020-08-24 16:27:44 +0900

    update(models) : product_model에서 create_product() 메소드 생성.

--- apps/common/models/member_model.py
+++ apps/common/models/member_model.py
@@ -6,30 +6,28 @@ from django.db import models
 from imagekit.models import ProcessedImageField
 
 
-
 class Member(models.Model):
-    id_member: int = models.AutoField(primary_key=True)
-    name: str = models.CharField(max_length=30)
-    user_id: str = models.CharField(unique=True, max_length=30)
-    user_pw: str = models.CharField(max_length=300)
-    nick_name: str = models.CharField(max_length=30)
-    tel: str = models.CharField(max_length=20)
-    birth = models.DateField(null=True, blank=True)
-    email: str = models.CharField(max_length=30, blank=True)
-    gender: str = models.CharField(max_length=6, blank=True)
-    cdate: datetime = models.DateTimeField(auto_now_add=True)
-    udate: datetime = models.DateTimeField(auto_now=False, null=True, blank=True)
-    last_date: datetime = models.DateTimeField(auto_now=False, null=True, blank=True)
-    image: ProcessedImageField = ProcessedImageField(
-        null=True,
-        upload_to="User",
-        format='JPEG',
-    )
+	id_member: int = models.AutoField(primary_key=True)
+	name: str = models.CharField(max_length=30)
+	user_id: str = models.CharField(unique=True, max_length=30)
+	user_pw: str = models.CharField(max_length=300)
+	nick_name: str = models.CharField(max_length=30)
+	tel: str = models.CharField(max_length=20)
+	birth = models.DateField(null=True, blank=True)
+	email: str = models.CharField(max_length=30, blank=True)
+	gender: str = models.CharField(max_length=6, blank=True)
+	cdate: datetime = models.DateTimeField(auto_now_add=True)
+	udate: datetime = models.DateTimeField(auto_now=False, null=True, blank=True)
+	last_date: datetime = models.DateTimeField(auto_now=False, null=True, blank=True)
+	image: ProcessedImageField = ProcessedImageField(
+		null=True,
+		upload_to="User",
+		format='JPEG',
+	)
 
-    class Meta:
-        db_table = 'member'
-        # app_label = 'common'
+	class Meta:
+		db_table = 'member'
 
-    def delete_image(self, *args, **kargs):
-        os.remove(os.path.join(settings.MEDIA_ROOT, self.file.path))
-        super().delete(*args, **kargs)
+	def delete_image(self, *args, **kargs):
+		os.remove(os.path.join(settings.MEDIA_ROOT, self.file.path))
+		super().delete(*args, **kargs)
