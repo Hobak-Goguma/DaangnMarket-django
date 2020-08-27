a66907d2c3fce4f0549fbbc6ae450ad78be0873d
ddusi <ddusi@kakao.com>
2020-08-28 04:32:49 +0900

    feature(models): my-list 페이지네이션 적용

--- apps/posts/views/product_my_list_view.py
+++ apps/posts/views/product_my_list_view.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import authentication, permissions
 
 from posts.models.product_model import Product
@@ -25,6 +26,17 @@ class ProductMyList(APIView):
 			}
 			return Response(content, status=status.HTTP_404_NOT_FOUND)
 
+		paginator = PageNumberPagination()
+
+		# 페이지 사이즈를 page_size라는 이름의 파라미터로 받을 거임
+		paginator.page_size_query_param = "page_size"
+
 		my_product = Product.objects.filter(id_member=id_member)
-		serializer = ProductSearchSerializer(my_product, many=True)
+		paginated_my_product = paginator.paginate_queryset(my_product, request)
+
+		if paginated_my_product is not None:
+			serializers = ProductSearchSerializer(paginated_my_product, many=True, context={'request': request})
+			return paginator.get_paginated_response(serializers.data)
+
+		serializer = ProductSearchSerializer(my_product, many=True, context={'request': request})
 		return Response(serializer.data, status=status.HTTP_200_OK)
