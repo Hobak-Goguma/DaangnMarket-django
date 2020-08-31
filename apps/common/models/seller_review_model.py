46135470e745f0342ce547be00904ad52ee7a25b
ddusi <ddusi@kakao.com>
2020-08-31 17:49:36 +0900

    feature(models): 모델 변수들 명시적으로 바꿈.

--- apps/common/models/seller_review_model.py
+++ apps/common/models/seller_review_model.py
@@ -1,12 +1,15 @@
+from datetime import datetime
+
 from django.db import models
+
+from common.models.member_model import Member
 
 
 class SellerReview(models.Model):
-    id_review_seller = models.AutoField(primary_key=True)
-    id_seller = models.ForeignKey('common.Member', models.DO_NOTHING, db_column='id_seller')
-    content = models.CharField(max_length=100)
-    cdate = models.DateTimeField(auto_now_add=True)
+	id_review_seller: int = models.AutoField(primary_key=True)
+	id_seller: Member = models.ForeignKey('common.Member', models.DO_NOTHING, db_column='id_seller')
+	content: str = models.CharField(max_length=100)
+	cdate: datetime = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        db_table = 'seller_review'
-        app_label = 'common'
+	class Meta:
+		db_table = 'seller_review'
