b1a97168649c6f1f30ffd35a08cea15148594ea6
ddusi <ddusi@kakao.com>
2020-09-11 13:02:46 +0900

    feature(schema): 특정멤버의 주소 API 스키마 생성 - 기존 request.body를 request.data로직으로 수정

--- apps/common/views/schema/member_addr_schema.py
+++ apps/common/views/schema/member_addr_schema.py
@@ -36,7 +36,7 @@ member_addr_schema_update = {
             'addr': openapi.Schema(type=openapi.TYPE_STRING, \
               description='멤버 설정 주소 (주소는 2개가지 설정 가능)'),
             'dis': openapi.Schema(type=openapi.TYPE_INTEGER, \
-              description='주소기준의 거리반경 (0, 2, 5, 10, 15)'),
+              description='주소기준의 거리반경 (0, 2, 5, 10, 15 단위 : km)'),
         }
 
 # Example
@@ -52,45 +52,4 @@ member_addr_example_update = {
 		'dis' : 2
 	}
 
-member_addr_get_example = {
-  "count": 3,
-  "next": "http://localhost:8000/member?page=2&page_size=1",
-  "previous": None,
-  "results": [
-    {
-      "id_member": 1,
-      "name": "김뚜띠",
-      "nick_name": "동이",
-      "user_id": "dduddi555",
-      "user_pw": "password",
-      "tel": "010-0000-0000",
-      "birth": "2000-05-04",
-      "email": "dduddi555@test.com",
-      "gender": "MALE",
-      "image": None,
-      "cdate": "2020-08-24T01:25:27.560103",
-      "udate": "2020-09-10T08:05:36.494970",
-      "last_date": None
-    }
-  ]
-}
-
-
-
-
 # custom response schema
-member_addr_get_schema = openapi.Schema(
-    'response',
-    type=openapi.TYPE_OBJECT,
-    properties={
-    'count': openapi.Schema(type=openapi.TYPE_INTEGER, \
-                           description='객체 갯수'),
-    'next': openapi.Schema(type=openapi.TYPE_INTEGER, \
-                           description='다음 페이지'),
-    'previous': openapi.Schema(type=openapi.TYPE_INTEGER, \
-                           description='이전 페이지'),
-    'results': openapi.Schema(type=openapi.TYPE_OBJECT, \
-                           properties=member_addr_schema)
-    },
-    example=member_addr_get_example
-)
