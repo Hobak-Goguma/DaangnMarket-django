e76993206ce873fa1932d03be6218eac336e93c3
ddusi <ddusi@kakao.com>
2020-10-02 00:11:20 +0900

    feature(views): allowany 권한설정  - member, nickname 중복확인,  - 시군구 읍면동 api  - test api  - location search product API

--- apps/common/views/test_views.py
+++ apps/common/views/test_views.py
@@ -1,3 +1,4 @@
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -11,6 +12,7 @@ class TestView(APIView):
 
 	"""
 	www_authenticate_realm = 'api'
+	permission_classes = [AllowAny]
 
 	def post(self, request):
 		# JWToken_user = j.get_user(self, j.get_validated_token(self, request.headers['Authorization'].split(' ')[1]))
