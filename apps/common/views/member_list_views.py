b1aebb5f53a3f65d0125ea55a7038fceb441977d
ddusi <58924238+ddusi@users.noreply.github.com>
2020-10-03 15:40:20 +0900

    Revert "[Feature] test issue"

--- apps/common/views/member_list_views.py
+++ apps/common/views/member_list_views.py
@@ -47,4 +47,3 @@ class MemberListView(APIView):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-# test
