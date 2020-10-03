570c2d379822af49a5e04f257f0ce107bf97fdd3
ddusi <ddusi@kakao.com>
2020-10-03 15:37:34 +0900

    feature(test): test

--- apps/common/views/member_list_views.py
+++ apps/common/views/member_list_views.py
@@ -47,3 +47,4 @@ class MemberListView(APIView):
 			serializer.save()
 			return Response(serializer.data, status=status.HTTP_201_CREATED)
 		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# test
