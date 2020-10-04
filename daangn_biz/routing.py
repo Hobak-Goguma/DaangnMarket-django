1a1617dedc537cfcd4ded05bc4a4354443e68cad
DevLeeSeonwoo <leeseonwoo5305@gmail.com>
2020-10-04 16:58:22 +0900

    feature(chat): ws token 작업 중

--- daangn_biz/routing.py
+++ daangn_biz/routing.py
@@ -1,12 +1,12 @@
 # daangn_biz/routing.py
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
-
+from chat.token_auth import TokenAuthMiddlewareStack
 import chat.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': AuthMiddlewareStack(
+    'websocket': TokenAuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
