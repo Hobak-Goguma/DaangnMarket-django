fcf40b0ee66a35b8acc3a9a8e1e503353829397f
DevLeeSeonwoo <leeseonwoo5305@gmail.com>
2020-10-09 19:20:10 +0900

    feature(chat): TokenMiddleware 작업

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,12 +4,19 @@ from channels.consumer import AsyncConsumer
 class EchoConsumer(AsyncConsumer):
 
     async def websocket_connect(self, event):
+        self.user = self.scope["user"]
         await self.send({
             "type": "websocket.accept",
+        })
+
+    async def websocket_disconnect(self, event):
+        await self.send({
+            "type": "websocket.accept"
         })
 
     async def websocket_receive(self, event):
         await self.send({
             "type": "websocket.send",
-            "text": event["text"],
+            "text": str(self.user) + ': ' + event["text"],
         })
+
--- daangn_biz/routing.py
+++ daangn_biz/routing.py
@@ -2,11 +2,12 @@
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from chat.token_auth import TokenAuthMiddlewareStack
+from chat.token import TokenAuthMiddleware
 import chat.routing
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
-    'websocket': TokenAuthMiddlewareStack(
+    'websocket': TokenAuthMiddleware(
         URLRouter(
             chat.routing.websocket_urlpatterns
         )
