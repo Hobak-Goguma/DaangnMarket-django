b3273b146460421b9fb6388239363f533f4c6d98
ddusi <ddusi@kakao.com>
2020-10-17 14:44:41 +0900

    Update(settings): jwt token refresh 만료시간 30일로 설정

--- daangn_biz/settings.py
+++ daangn_biz/settings.py
@@ -81,7 +81,7 @@ REST_FRAMEWORK = {
 # JWToken
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
 
